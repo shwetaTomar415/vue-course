8043c27756c6fe70d22f9ee602048c7744ed073d
Peeyush Sengar <peeyushsengar415@gmail.com>
2022-10-16 01:17:35 +0530

    Event Handle Counter

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,21 @@
         <div class="col">
           <p class="h3">App Component</p>
           <p class="fst-italic"></p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </div>
       </div>
     </div>
-
+    <Counter />
   </div>
 </template>
 
 
 <script>
 
+import Counter from "@/components/Counter";
 export default {
-  components: {}
+  components: {Counter},
+
 }
 </script>
 <style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card shadow-lg">
+          <div class="card-body">
+            <p class="h3 display-3">{{count}}</p>
+              <button class="btn btn-success m-1" @click="increment">Increment</button>
+              <button class="btn btn-warning m-1" @click="decrement">Decrement</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data(){
+    return{
+      count:0,
+    }
+  },
+  methods:{
+    increment(){
+      this.count++;
+    },
+    decrement(){
+      this.count--;
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
