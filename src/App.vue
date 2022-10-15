336a465784885e28344045a825ee9ac22d4aa350
Peeyush Sengar <peeyushsengar415@gmail.com>
2022-10-16 00:34:12 +0530

    Update: Props Data Binding

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
-<div>
-  <h2>Hello: BootStrap classes Added</h2>
-  <button class="btn btn-success">Success</button>
-</div>
+  <div class="container">
+    <div class="grid">
+      <div class="row">
+        <div class="col">
+          <p class="h3 text-success fw-bold">App Component</p>
+          <p class="fst-italic">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+      </div>
+    </div>
+    <Customer name="Rajan" age=25 designation="Software Engineer" />
+
+    <Employee />
+  </div>
 </template>
 
 
 <script>
+import Customer from "@/components/Customer";
+import Employee from "@/components/Employee";
+export default {
+  components: {Employee, Customer}
+}
 </script>
 <style>
 @import "./style.css";
---
+++ src/components/Employee.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <p class="h3">Employee</p>
+      <ul class="list-group">
+        <li class="list-group-item">Name : {{name}}</li>
+        <li class="list-group-item">Age : {{age}}</li>
+        <li class="list-group-item">Designation :  {{designation}}</li>
+      </ul>
+
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Employee",
+  data(){
+    return{
+      name:'Rama',
+      age:26,
+      designation:'Software Engineer'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
