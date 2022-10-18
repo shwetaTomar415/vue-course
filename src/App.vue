b8495f719d8869534aa118c7e540609ba3216e4a
Peeyush Sengar <peeyushsengar415@gmail.com>
2022-10-19 00:10:17 +0530

    simple form handling

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,16 @@
         </div>
       </div>
     </div>
-
+    <Register />
   </div>
 </template>
 
 
 <script>
 
+import Register from "@/components/Register";
 export default {
-  components: {}
+  components: {Register}
 }
 </script>
 <style>
---
+++ src/components/Register.vue
@@ -0,0 +1,54 @@
+<template>
+<!--  <pre>{{user}}</pre>-->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="card shadow-lg">
+          <div class="card-header bg-success text-white">
+          <p class="h3">Register Here</p>
+        </div>
+          <div class="card-body">
+          <form @submit.prevent="register()">
+            <div class="mb-2">
+              <input v-model="user.name" class="form-control" placeholder="Name" type="text" />
+            </div>
+            <div class="mb-2">
+              <input v-model="user.email" class="form-control" placeholder="Email" type="email" />
+            </div>
+            <div class="mb-2">
+              <input v-model="user.password" class="form-control" placeholder="Password" type="password" />
+            </div>
+            <div class="mb-2">
+              <input class="btn btn-success" value="Register"  type="submit" />
+            </div>
+          </form>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+  data(){
+    return{
+      user:{
+        name:'',
+        email:'',
+        password:'',
+      }
+    }
+  },
+  methods:{
+    register(){
+      console.log(this.user);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
